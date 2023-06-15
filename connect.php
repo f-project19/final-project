76eed08e0c13b50bc44b18fd6c3e3baea694684e
xinfengkk <130195187+xinfengkk@users.noreply.github.com>
2023-06-15 23:11:17 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("连接数据库失败：" . $conn->connect_error);
+}
+?>
